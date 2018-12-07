a3af6386c930a45a9f39cce898382e9074ffc88f
mloweedgar <mloweedgar0@gmail.com>
2018-12-07 11:13:59 +0300

    Closes #59: Add Full screen control for Alert Details map

--- src/Alerts/Alert/components/AlertMap/index.js
+++ src/Alerts/Alert/components/AlertMap/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import * as ReactLeaflet from 'react-leaflet';
+import L from 'leaflet';
+import 'leaflet.fullscreen/Control.FullScreen';
+import 'leaflet.fullscreen/Control.FullScreen.css';
 import classnames from 'classnames/bind';
 import styles from './styles.css';
 
@@ -17,13 +20,37 @@ const { Map: LeafletMap, TileLayer } = ReactLeaflet;
  * @since 0.1.0
  */
 class AlertMap extends React.Component {
-  componentDidMount() {}
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      fullscreen: false
+    }
+    this.alertMapRef = React.createRef();
+  }
+
+  initilizeFullScreen = () => {
+    L.control.fullscreen({
+      position: 'topleft',
+      title: 'Show me the fullscreen !',
+      titleCancel: 'Exit fullscreen mode',
+      content: null,
+      forceSeparateButton: true,
+      forcePseudoFullscreen: true,
+      fullscreenElement: false
+    }).addTo(this.map);
+  }
+  componentDidMount() {
+    this.map = this.alertMapRef.current.leafletElement;
+    this.initilizeFullScreen();
+  
+  }
 
   render() {
     const position = [-6.179, 35.754];
     return (
       <div className={cx('AlertMap')}>
-        <LeafletMap center={position} zoom={7}>
+        <LeafletMap center={position} zoom={6} ref={this.alertMapRef}>
           <TileLayer
             attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
             id="mapbox.light"
